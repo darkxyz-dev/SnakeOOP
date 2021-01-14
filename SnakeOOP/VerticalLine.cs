6892cbb66c38479d656299d938f704295504cede
Jura Golubev <darkyxyz12@gmail.com>
2021-01-14 16:15:19 +0200

    first commit

---
+++ SnakeOOP/VerticalLine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeOOP
+{
+    class VerticalLine : Figure
+    {
+        
+
+
+        public VerticalLine(int yUP, int yDown, int x, char symb)
+        {
+            pointList = new List<Point>();
+            for(int i = yUP; i <= yDown; i++)
+            {
+                Point p = new Point(x, i, symb);
+                pointList.Add(p);
+            }
+        }
+
+        
+    }
+
+}
